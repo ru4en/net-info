1fda866c6fae097a6c6eec39f3b63aa0e19d9826
ruben lopes <ru4en@pm.me>
2022-10-10 22:59:22 +0100

    Adding scelton code

---
+++ README.md
@@ -0,0 +1,9 @@
+# NET INFO
+
+net-info is a simple networking application to request infomation about the clients network. It can also be used as a diagnostics tool.
+
+- Local IP
+- Public IP
+- Open Ports
+- Check If Internet is working
+- Speed Test
---
+++ main.py
@@ -0,0 +1,59 @@
+
+
+def setupConf():
+    """
+    Creates Configs files
+    and set default values
+    """
+    pass
+
+def getConf():
+    """
+    Get all configs data stored at ...
+    """
+    pass
+
+def getPubIP():
+    """
+    Get the Public IP address of the network the device is conected to
+    """
+    return 0
+
+def getLoclIP():
+    """
+    Get the devices local IP adrress
+    """
+    return 0
+
+def getIPloc():
+    """
+    Return the IP location of the network
+    """
+    return
+
+def getDeviOnNet():
+    """
+    return the number of devices on the network
+    """
+    return 0
+
+def scanForOpenPorts():
+    """
+    Return all the open ports of the current device
+    """
+    return 0
+
+def checkCon():
+    """
+    Test to see if device conectes to the internet
+    """
+    return True
+
+def speedTest():
+    """
+    Run and return the results of a network speed test
+    """
+    return 0
+
+def main():
+    pass
